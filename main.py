8b45561b58fa3966185c6412475f8920f7d5d615
Elim Quijano <elim1905quijano@gmail.com>
2025-02-17 12:25:24 -0500

    fix syntax error

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
+# -*- coding: utf-8 -*-
 import socket
 import threading
 
 def handle_client(conn, addr):
-    print(f"Conexión establecida por {addr}")
+    print("Conexión establecida por {}".format(addr))
     while True:
         data = conn.recv(1024)  # Recibir datos
         if not data:
             break  # Salir si no hay más datos
-        print(f"Datos recibidos de {addr}: {data.decode('utf-8')}")
+        print("Datos recibidos de {}: {}".format(addr, data.decode('utf-8')))
         # Aquí puedes procesar los datos como desees
     conn.close()
 
 def start_tcp_server(port):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind(('0.0.0.0', port))
-        s.listen()
-        print(f"Servidor TCP escuchando en el puerto {port}")
-        while True:
-            conn, addr = s.accept()
-            threading.Thread(target=handle_client, args=(conn, addr)).start()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('0.0.0.0', port))
+    s.listen(5)
+    print("Servidor TCP escuchando en el puerto {}".format(port))
+    while True:
+        conn, addr = s.accept()
+        threading.Thread(target=handle_client, args=(conn, addr)).start()
 
 # Iniciar servidores TCP en los puertos 5055 y 5056
 threading.Thread(target=start_tcp_server, args=(5001,)).start()
